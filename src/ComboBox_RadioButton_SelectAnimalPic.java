4d1b5e5236a4a9f2a577c53891966cc1214ca207
koo8 <kooeight@gmail.com>
2020-07-22 08:49:34 -0500

    update ComboBox_RadioButton_SelectAnimalPic.java with a ListCellRender to include a Class(Pet) with new background color on cells

--- src/ComboBox_RadioButton_SelectAnimalPic.java
+++ src/ComboBox_RadioButton_SelectAnimalPic.java
@@ -16,7 +16,15 @@ public class ComboBox_RadioButton_SelectAnimalPic extends JPanel implements Acti
     JComboBox comboBox;
     ButtonGroup buttonGroup;
     JRadioButton button;
-    String[] petNames = {"bird", "cat", "dog", "rabbit", "Pig"};
+   // String[] petNames = {"bird", "cat", "dog", "rabbit", "Pig"};
+    Pet[] pets  = {
+            new Pet("cat", 3, "like to eat fish"),
+           new Pet("bird", 5, "fly high"),
+           new Pet("dog", 7, "bark constantly"),
+           new Pet("rabbit", 8, "hopping around"),
+           new Pet("Pig", 1, "sleep and eat only")
+
+   };
     JLabel pictureCombox;
 
     //Constructor
@@ -26,7 +34,12 @@ public class ComboBox_RadioButton_SelectAnimalPic extends JPanel implements Acti
         // combobox - non-editable
         // the following two lines both work
         // comboBox = new JComboBox<String >(petNames);
-        comboBox = new JComboBox(petNames);
+       // comboBox = new JComboBox(petNames);
+
+        // create a comboBox with renderer for a new Class Pet
+        comboBox = new JComboBox(pets);
+        comboBox.setRenderer(new BackgroundColorRenderer());
+        comboBox.setMaximumRowCount(3);
         comboBox.setActionCommand("combo");
         comboBox.addActionListener(this);
 
@@ -53,8 +66,8 @@ public class ComboBox_RadioButton_SelectAnimalPic extends JPanel implements Acti
         JRadioButton[] buttons = new JRadioButton[5];
         for (int i = 0; i <buttons.length ; i++) {
             JRadioButton b = new JRadioButton();
-            b.setText(petNames[i]);
-            b.setActionCommand(petNames[i]);
+            b.setText(pets[i].getPetName());
+            b.setActionCommand(pets[i].getPetName());
             b.addActionListener(this);
             buttonGroup.add(b);
             bottomPanel.add(b);
@@ -69,7 +82,7 @@ public class ComboBox_RadioButton_SelectAnimalPic extends JPanel implements Acti
 
     private void updatePictureLabel(int index) {
 
-        String name = petNames[index];
+        String name = pets[index].getPetName();
         ImageIcon icon = Utility.createImageIcon(this, "images/" + name + ".gif");
         if (icon != null) {
             pictureCombox.setIcon(icon);
@@ -119,6 +132,55 @@ public class ComboBox_RadioButton_SelectAnimalPic extends JPanel implements Acti
             pictureCombox.setIcon(Utility.createImageIcon(
                     this,"images/" + command + ".gif"
             ));
+        }
+    }
+
+    /**
+     * ListCellRenderer can define a PreferredSize of the Cell
+     * or Use DefaultListCellRender construct approach create a renderer
+     * extends JLabel impliments ListCellRenderer ( see oracle ComboBoxDemo.java)
+     */
+    private class BackgroundColorRenderer implements ListCellRenderer {
+        @Override
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+
+              JLabel renderer = (JLabel) new DefaultListCellRenderer().getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+              Pet pet = (Pet) value;
+              String name = pet.getPetName();
+              String fav = pet.getFavActivity();
+              int age = pet.getAge();
+              renderer.setText(String.format("%s - %d - %s", name, age, fav));
+
+              renderer.setBackground(index %2 == 0 ? Color.BLUE: (Color) UIManager.get("List.background"));
+              renderer.setFont(new Font("serif", Font.ITALIC, 24));
+              renderer.setPreferredSize(new Dimension(0, 30));
+              renderer.setForeground(Color.getHSBColor(0.4f, 0.7f, 0.9f));
+              return renderer;
+
+        }
+    }
+
+    private class Pet {
+
+        String petName;
+        int age;
+        String favActivity;
+        public Pet(String petName, int age, String fav ) {
+            this.petName = petName;
+            this.age = age;
+            this.favActivity = fav;
+        }
+
+        public String getPetName() {
+            return petName;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public String getFavActivity() {
+            return favActivity;
         }
     }
 }
